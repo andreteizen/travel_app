985e7b708b38c0661701ee768643c8fcab8944fb
Andre <andre.teizen@gmail.com>
2024-09-23 17:38:21 -0300

    Ajuste footer

--- constants/index.ts
+++ constants/index.ts
@@ -66,14 +66,6 @@ export const FOOTER_LINKS = [
   },
 ];
 
-export const FOOTER_CONTACT_INFO = {
-  title: 'Contact Us',
-  links: [
-    { label: 'Admin Officer', value: '123-456-7890' },
-    { label: 'Email Officer', value: 'hilink@akinthil.com' },
-  ],
-};
-
 export const SOCIALS = {
   title: 'Social',
   links: [
